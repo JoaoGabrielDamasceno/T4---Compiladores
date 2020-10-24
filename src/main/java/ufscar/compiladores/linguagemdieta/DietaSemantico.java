5d80c22a6327545f9bce190bd55bad00cbdec03c
Sergio Ricardo Hideki Nisikava <sergionisikava@gmail.com>
2020-10-24 13:20:04 -0300

    Documentação do gerador e arrumado identação

--- src/main/java/ufscar/compiladores/linguagemdieta/DietaSemantico.java
+++ src/main/java/ufscar/compiladores/linguagemdieta/DietaSemantico.java
@@ -5,10 +5,6 @@ import ufscar.compiladores.linguagemdieta.linguagemDIETAParser;
 import java.util.HashMap;
 import java.util.List;
 
-/**
- *
- * @author joaog
- */
 public class DietaSemantico extends linguagemDIETABaseVisitor<Object>{
     //String global_refeicao = null;
     Escopos escoposAninhados = new Escopos();
@@ -31,8 +27,8 @@ public class DietaSemantico extends linguagemDIETABaseVisitor<Object>{
         String verificaNull = ctx.cadeia.getText();
         
         if(verificaNull.equals("\"\"")){
-             System.out.println("[ERRO SEMANTICO]" + "Linha " + ctx.getStart().getLine() + ": nome do profissional não  inserido!");
-             erroSemantico = true;
+            System.out.println("[ERRO SEMANTICO]" + "Linha " + ctx.getStart().getLine() + ": nome do profissional não  inserido!");
+            erroSemantico = true;
         }
     return null;
     }
@@ -42,8 +38,8 @@ public class DietaSemantico extends linguagemDIETABaseVisitor<Object>{
         String verificaNull = ctx.cadeia.getText();
         
         if(verificaNull.equals("\"\"")){
-             System.out.println("[ERRO SEMANTICO]" + "Linha " + ctx.getStart().getLine() + ": nome do paciente não  inserido!");
-             erroSemantico = true;
+            System.out.println("[ERRO SEMANTICO]" + "Linha " + ctx.getStart().getLine() + ": nome do paciente não  inserido!");
+            erroSemantico = true;
         }
     return null;
     }
@@ -64,27 +60,25 @@ public class DietaSemantico extends linguagemDIETABaseVisitor<Object>{
     
     @Override
     public Void visitDieta(linguagemDIETAParser.DietaContext ctx){
-            visitCorpo(ctx.corpo());
-        return null;
+        visitCorpo(ctx.corpo());
         
+        return null;   
     }
     
     //Verifica se não tem refeicão repetida, ou seja, declarada duas vezes
     @Override
     public Void visitRefeicao(linguagemDIETAParser.RefeicaoContext ctx){
-            if (tabelas.verificar(ctx.ident_refeicao.getText()) != null ) {
-                System.out.println("[ERRO SEMANTICO]" + "Linha" + ctx.ident_refeicao.getStart().getLine() + ":" + "Refeição " + ctx.ident_refeicao.getText() + " já declarada!");
-                erroSemantico = true;
-            } else {
-         
+        if (tabelas.verificar(ctx.ident_refeicao.getText()) != null ) {
+            System.out.println("[ERRO SEMANTICO]" + "Linha" + ctx.ident_refeicao.getStart().getLine() + ":" + "Refeição " + ctx.ident_refeicao.getText() + " já declarada!");
+            erroSemantico = true;
+        } else {
             tabelas.inserir(ctx.ident_refeicao.getText());
         }
-           escoposAninhados.criarNovoEscopo();
-           visitOpcao_alimentos(ctx.opcao_alimentos());
-           escoposAninhados.abandonarEscopo();
+        escoposAninhados.criarNovoEscopo();
+        visitOpcao_alimentos(ctx.opcao_alimentos());
+        escoposAninhados.abandonarEscopo();
            
-        return null;
-        
+        return null;   
     }
     
     //Verifica se dentro da mesma refeição, uma opção de alimentação foi declarada duas vezes igual (muito dificil mas foi feita verificação desse erro semantico
@@ -92,31 +86,24 @@ public class DietaSemantico extends linguagemDIETABaseVisitor<Object>{
     public Void visitOpcao_alimentos(linguagemDIETAParser.Opcao_alimentosContext ctx){
         TabelaSimbolos escopoAtual = escoposAninhados.obterEscopoAtual();
         
-         if (escopoAtual.verificar(ctx.a1.getText()) != null) {
+        if (escopoAtual.verificar(ctx.a1.getText()) != null) {
             System.out.println("[ERRO SEMANTICO]" + "Linha "  + ctx.a1.getStart().getLine() + ":" + "Opção de alimentação " + ctx.a1.getText() + " já declarada na mesma refeição!");
             erroSemantico = true;
-         }
-         else{
-             escopoAtual.inserir(ctx.a1.getText());
-             escoposAninhados.criarNovoEscopo();
-             visitAlimentos(ctx.a1);
-             escoposAninhados.abandonarEscopo();
-                    
-         }
-         
-            for (linguagemDIETAParser.AlimentosContext alimentosCtx : ctx.outrosA1) {
-                 if (escopoAtual.verificar(alimentosCtx.getText()) != null) {
-                     System.out.println("[ERRO SEMANTICO]" + "Linha "  + alimentosCtx.getStart().getLine() + ":" + "Opção de alimentação " + alimentosCtx.getText() + " já declarada na mesma refeição!");
-                     erroSemantico = true;
-                 }
-                else{
-                    escopoAtual.inserir(alimentosCtx.getText());    
-                }
-            
-            }
-            
-            visitAlimentos(ctx.alimentos);
-            
+        } else{
+            escopoAtual.inserir(ctx.a1.getText());
+            escoposAninhados.criarNovoEscopo();
+            visitAlimentos(ctx.a1);
+            escoposAninhados.abandonarEscopo();             
+        }
+        for (linguagemDIETAParser.AlimentosContext alimentosCtx : ctx.outrosA1) {
+            if (escopoAtual.verificar(alimentosCtx.getText()) != null) {
+                System.out.println("[ERRO SEMANTICO]" + "Linha "  + alimentosCtx.getStart().getLine() + ":" + "Opção de alimentação " + alimentosCtx.getText() + " já declarada na mesma refeição!");
+                erroSemantico = true;
+            } else{
+                escopoAtual.inserir(alimentosCtx.getText());    
+            } 
+        }
+        visitAlimentos(ctx.alimentos);   
         
         return null;
     }
@@ -127,29 +114,22 @@ public class DietaSemantico extends linguagemDIETABaseVisitor<Object>{
         TabelaSimbolos escopoAtual = escoposAninhados.obterEscopoAtual();
         
         if (escopoAtual.verificar(ctx.a2.cadeia.getText()) != null) {
-                System.out.println("[ERRO SEMANTICO]" + "Linha "  + ctx.a2.getStart().getLine() + ":" + " Alimento " + ctx.a2.cadeia.getText() + " já declarada na mesma opção da refeição!");
-                erroSemantico = true;
+            System.out.println("[ERRO SEMANTICO]" + "Linha "  + ctx.a2.getStart().getLine() + ":" + " Alimento " + ctx.a2.cadeia.getText() + " já declarada na mesma opção da refeição!");
+            erroSemantico = true;
+        } else{
+            escopoAtual.inserir(ctx.a2.cadeia.getText());            
         }
-         else{
-             escopoAtual.inserir(ctx.a2.cadeia.getText());
-             
-                    
-         }
-         
-            for (linguagemDIETAParser.AlimentoContext alimentoCtx : ctx.outrosA2) {
-                 if (escopoAtual.verificar(alimentoCtx.cadeia.getText()) != null) {
-                    System.out.println("[ERRO SEMANTICO]" + "Linha "  + alimentoCtx.getStart().getLine() + ":" + " Alimento " + alimentoCtx.cadeia.getText() + " já declarada na mesma opção da refeição!");
-                    erroSemantico = true;
-                 }
-                else{
-                    escopoAtual.inserir(alimentoCtx.cadeia.getText());    
-                }
-            
-            }
+        for (linguagemDIETAParser.AlimentoContext alimentoCtx : ctx.outrosA2) {
+            if (escopoAtual.verificar(alimentoCtx.cadeia.getText()) != null) {
+                System.out.println("[ERRO SEMANTICO]" + "Linha "  + alimentoCtx.getStart().getLine() + ":" + " Alimento " + alimentoCtx.cadeia.getText() + " já declarada na mesma opção da refeição!");
+                erroSemantico = true;
+            } else{
+                escopoAtual.inserir(alimentoCtx.cadeia.getText());    
+            }     
+        }
 
-    return null;
+        return null;
     }
-
 }
 
 
--- src/main/java/ufscar/compiladores/linguagemdieta/GeradorHTML.java
+++ src/main/java/ufscar/compiladores/linguagemdieta/GeradorHTML.java
@@ -11,8 +11,8 @@ public class GeradorHTML extends linguagemDIETABaseVisitor<Object>{
     }
     
     @Override
-    public Void visitFicha(linguagemDIETAParser.FichaContext ctx) {
-        
+    public Void visitFicha(linguagemDIETAParser.FichaContext ctx) {       
+        //Declaração do corpo do HTML, com styles
         saida.append("<!DOCTYPE html>");
         saida.append("<html>");
         saida.append("<style>");
@@ -23,17 +23,22 @@ public class GeradorHTML extends linguagemDIETABaseVisitor<Object>{
         saida.append("</style>");
         saida.append("<body>");
         
+        /*A ficha é composta por cabeçalho, dieta e rodape, nessa ordem.
+        então primeiros incluimos cabeçalho, depois dieta e depois rodape*/
         visitCabeçalho(ctx.cabeçalho());
         visitDieta(ctx.dieta());
         visitRodape(ctx.rodape());
         
+        //Fim do HTML
         saida.append("</body>");
         saida.append("</html>");
         return null;
     }
     
     @Override
-    public Void visitCabeçalho(linguagemDIETAParser.CabeçalhoContext ctx){
+    public Void visitCabeçalho(linguagemDIETAParser.CabeçalhoContext ctx){        
+        /*O cabeçalho é composto por nome do profissional, CRN, especialidade, nome do
+        cliente e data de emissão da ficha*/
         visitProfissional(ctx.profissional());
         visitCrn(ctx.crn());
         visitEspecialidade(ctx.especialidade());
@@ -43,7 +48,8 @@ public class GeradorHTML extends linguagemDIETABaseVisitor<Object>{
     }
     
     @Override
-    public Void visitProfissional(linguagemDIETAParser.ProfissionalContext ctx){
+    public Void visitProfissional(linguagemDIETAParser.ProfissionalContext ctx){       
+        //Inclui o nome do profissional na ficha
         saida.append("<biggerFont>");
         saida.append("<b>");
         saida.append("<center>");
@@ -57,39 +63,51 @@ public class GeradorHTML extends linguagemDIETABaseVisitor<Object>{
     
     @Override
     public Void visitCrn(linguagemDIETAParser.CrnContext ctx){
+        //Inclui o CRN do profissional na ficha
         saida.append("<center>CRN-");
+        //CRN possui dígito de 1 a 10
         saida.append(ctx.digito.getText());
         saida.append(" ");
+        //CRN possui um codigo para cada profissional
         visitNum_identificador(ctx.num_identificador());
+        
         return null;
     }
     
     @Override
     public Void visitNum_identificador(linguagemDIETAParser.Num_identificadorContext ctx){
+        
+        //Inclui o código do profissional na ficha
         saida.append(" ");
         saida.append(ctx.identificador.getText());
+        
         return null;
     }
     
     @Override
     public Void visitEspecialidade(linguagemDIETAParser.EspecialidadeContext ctx){
+        //Inclui a especialidade do profissional na ficha
         saida.append(" ");
         saida.append(ctx.cadeia.getText().replace("\"", ""));
         saida.append("</center>");
+        
         return null;
     }
     
     @Override
     public Void visitCliente(linguagemDIETAParser.ClienteContext ctx){
+        //Inclui o nome do cliente na ficha
         saida.append("<p><b>");
         saida.append("Cliente: ");
         saida.append(ctx.cadeia.getText().replace("\"", ""));
         saida.append("</b></p>");
+        
         return null;
     }
     
     @Override
     public Void visitData(linguagemDIETAParser.DataContext ctx){
+        //Inclui a data de emissão da ficha na ficha
         saida.append("<p><b>Data: ");
         saida.append(ctx.dia.getText());
         saida.append("/");
@@ -97,11 +115,13 @@ public class GeradorHTML extends linguagemDIETABaseVisitor<Object>{
         saida.append("/");
         saida.append(ctx.ano.getText());
         saida.append("</b></p>");
+        
         return null;
     }
     
     @Override
     public Void visitRodape(linguagemDIETAParser.RodapeContext ctx){
+        //O rodape é composto pelo endereço e telefone
         visitEndereco(ctx.endereco());
         visitTelefone(ctx.telefone());
         return null;
@@ -109,6 +129,7 @@ public class GeradorHTML extends linguagemDIETABaseVisitor<Object>{
      
     @Override
     public Void visitEndereco(linguagemDIETAParser.EnderecoContext ctx){
+        //Inclui o endereço no rodape da ficha
         saida.append("<center><p>");
         saida.append(ctx.cadeia.getText().replace("\"", ""));
         saida.append(" - ");
@@ -117,6 +138,7 @@ public class GeradorHTML extends linguagemDIETABaseVisitor<Object>{
     
     @Override
     public Void visitTelefone(linguagemDIETAParser.TelefoneContext ctx){
+        //Inclui o telefone no rodape da ficha
         visitDdd(ctx.ddd());
         saida.append(ctx.digito1.getText());
         saida.append("-");
@@ -126,6 +148,7 @@ public class GeradorHTML extends linguagemDIETABaseVisitor<Object>{
     }
     @Override
     public Void visitDdd(linguagemDIETAParser.DddContext ctx){
+        //Inclui o DDD no telefone da ficha
         saida.append("Tel.(");
         saida.append(ctx.digito3.getText());
         saida.append(") ");
@@ -134,13 +157,16 @@ public class GeradorHTML extends linguagemDIETABaseVisitor<Object>{
     
     @Override
     public Void visitDieta(linguagemDIETAParser.DietaContext ctx){
+        //O corpo é composto por refeição
         visitCorpo(ctx.corpo());
         return null;
     }
     
     @Override
     public Void visitRefeicao(linguagemDIETAParser.RefeicaoContext ctx){
-
+        /*Refeicao é composta pelo nome da refeição e seus alimentos.
+        E inclui refeição na ficha
+        */
         saida.append("<table><td>");
         saida.append(ctx.ident_refeicao.getText().replace("_", " "));
         visitOpcao_alimentos(ctx.opcao_alimentos());
@@ -152,10 +178,12 @@ public class GeradorHTML extends linguagemDIETABaseVisitor<Object>{
     @Override
     public Void visitOpcao_alimentos(linguagemDIETAParser.Opcao_alimentosContext ctx){
         
+        //Inclui a primeira opção de dieta da refeição na ficha
         saida.append("<ul><li>");
         saida.append(ctx.a1.getText().replace("\"", "").replace("+", " + ").replace("-"," "));
         saida.append("</li></ul>");
         
+        //Inclui as demais opções de dieta da refeição na ficha
         for(linguagemDIETAParser.AlimentosContext alimentosCtx : ctx.outrosA1){
             saida.append("<ul><li>");
             saida.append(alimentosCtx.getText().replace("\"", "").replace("+", " + ").replace("-"," "));
